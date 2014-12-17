1600512f51d5e5c16e71c6dbdbc10cfacbdc069d
JeanMabru <jeanmabru_90@yahoo.fr>
2014-12-17 16:46:00 +0100

    Debut affichage grid
    Affichage de hello world pr le moment

--- WebContent/js/bship_grid.js
+++ WebContent/js/bship_grid.js
@@ -1,17 +1,27 @@
 
 //function show_grid()
 //{
+	var i;
+	var j; 
 	var grid = document.getElementById('bship_grid');
 	//grid.innerHTML = "TESTERINO22222222";
 	//alert("coucou");
+	grid.innerHTML = grid.innerHTML + "<tr>";
 	$.get("bsg",
 			{		
 			},
 			function(data, status){
 				//alert("sup");
-				alert(data.grid_size);
+				for(i = 0; i < data.grid_size; i++)
+					{
+						for(j = 0; j < data.grid_size; j++)
+							{
+								grid.innerHTML = grid.innerHTML + "<td> hello world </td>"; 
+							}
+					}
 			}
 	);
+	grid.innerHTML = grid.innerHTML + "</tr>";
 	
 	
 	
