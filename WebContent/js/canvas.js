93b4c49437f5c1d20a45f9df0c8ffc81dc5bb54b
JeanMabru <jeanmabru_90@yahoo.fr>
2015-01-06 23:53:11 +0800

    recuperation de la position souris sur le canvas

--- WebContent/js/canvas.js
+++ WebContent/js/canvas.js
@@ -1,6 +1,10 @@
 //la taille d'une case sera de 35*35
 var i_case_size = 35;
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
 
+window.addEventListener('mousedown', alert_pos, false);
+window.addEventListener('mousedown', getMousePos, false);
 
 // width="500" height="500"
 //style="border:1px solid #000000;"
@@ -10,28 +14,44 @@ var i_case_size = 35;
 function draw_grid(size)
 {
 	var x = 0,y = 0;
-	var c = document.getElementById("myCanvas");
-	var ctx = c.getContext("2d");
-	c.width = size*i_case_size;
-	c.height = size*i_case_size;
 	
-	for(x = 0; x < c.width ; x = x+i_case_size)
+	
+	canvas.width = size*i_case_size;
+	canvas.height = size*i_case_size;
+
+	
+	for(x = 0; x < canvas.width ; x = x+i_case_size)
 	{
 		ctx.moveTo(x,0);
-		ctx.lineTo(x,c.width);
+		ctx.lineTo(x,canvas.width);
 		ctx.stroke();
 	}
-	for(y = 0; y < c.width ; y = y+i_case_size)
+	for(y = 0; y < canvas.width ; y = y+i_case_size)
 	{
 		ctx.moveTo(0,y);
-		ctx.lineTo(c.width,y);
+		ctx.lineTo(canvas.width,y);
 		ctx.stroke();
 	}
 
 }
 
-function grid_click()
-{
-	
 
+function alert_pos(e)
+{
+	var pos = getMousePos(e);
+    posx = pos.x;
+    posy = pos.y;
+	alert(posx);
+	alert(posy);
 }
+
+
+
+function getMousePos(evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
+
