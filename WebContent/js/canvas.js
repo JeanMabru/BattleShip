4ef21743bd6bcf2a97d7f2a172ab7281ba9e8503
JeanMabru <jeanmabru_90@yahoo.fr>
2015-01-07 22:06:17 +0800

    envoie de l'array en JSON

--- WebContent/js/canvas.js
+++ WebContent/js/canvas.js
@@ -15,12 +15,12 @@ function draw_grid(size)
 {
 	var x = 0,y = 0;
 	var i = 0;
-	
-	
+				
+				
 	canvas.width = size*i_case_size;
 	canvas.height = size*i_case_size;
-
-	
+			
+				
 	for(x = 0; x < canvas.width ; x = x+i_case_size)
 	{
 		ctx.moveTo(x,0);
@@ -33,24 +33,24 @@ function draw_grid(size)
 		ctx.lineTo(canvas.width,y);
 		ctx.stroke();
 	}
-		x = 0;
-		y = 0;
-		do{
-			for(x = 0; x < canvas.width; x = x + i_case_size)
-			{
-				var grid_array_case = {x:0, y:0, status:0};
-				grid_array_case.x = x;
-				grid_array_case.y = y;
-				grid_array_case.status = 0;
-				grid_array.push(grid_array_case);
-				//alert("x is equal to " + x);
-				if(x + i_case_size == canvas.width){
-					y = y + i_case_size;
-					//alert("y is equal to " + y);
-				}
+	x = 0;
+	y = 0;
+	do{
+		for(x = 0; x < canvas.width; x = x + i_case_size)
+		{
+			var grid_array_case = {x:0, y:0, status:0};
+			grid_array_case.x = x;
+			grid_array_case.y = y;
+			grid_array_case.status = 0;
+			grid_array.push(grid_array_case);
+			//alert("x is equal to " + x);
+			if(x + i_case_size == canvas.width){
+				y = y + i_case_size;
+				//alert("y is equal to " + y);
 			}
-		}while(y<canvas.width);
-		
+		}
+	}while(y<canvas.width);
+					
 		/*for(i = 0; i < grid_array.length; i++)
 		{
 			alert("Grid " + i + "(x;y;status) is (" + grid_array[i].x + ";" + grid_array[i].y + ";" + grid_array[i].status + ")");			
@@ -69,11 +69,11 @@ function alert_pos(e)
 
 function change_status(e)
 {
-	grid_array[5].status = 1;
+	/*grid_array[5].status = 1;
 	grid_array[6].status = 1;
 	grid_array[7].status = 1;
 	grid_array[19].status = 1;
-	grid_array[20].status = 1;
+	grid_array[20].status = 1;*/
 	
 	var pos = getMousePos(e);
 	posx = pos.x;
@@ -96,8 +96,35 @@ function change_status(e)
 			ctx.fill();
 		}
 	}
-
-
+	//on envoie notre array au servlet en JSON. 
+	//on adapte le format de l'array pour que le stringify fonctionne
+	var grid_JSON = [];
+	function func_grid_JSON()
+	{
+		return{
+			JSONx:"",
+			JSONy:"",
+			JSONstatus:"",
+		}
+	};
+	for(i = 0; i < grid_array.length; i++)
+	{
+		var gr = func_grid_JSON();
+		
+		gr.JSONx = grid_array[i].x;
+		gr.JSONy = grid_array[i].y;
+		gr.JSONstatus = grid_array[i].status;
+		grid_JSON.push(gr);
+	}
+	
+	
+	$.post("bsg",
+		{
+			grid: JSON.stringify(grid_JSON),
+		});
+			
+	
+	
 }
 
 
--- src/bship_gridServlet/Bship_gridServlet.java
+++ src/bship_gridServlet/Bship_gridServlet.java
@@ -11,6 +11,9 @@ import javax.servlet.http.HttpServletResponse;
 import bship_classes.Bship_grid_global;
 
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
 
 public class Bship_gridServlet extends HttpServlet {
 
@@ -29,21 +32,34 @@ public class Bship_gridServlet extends HttpServlet {
 		super.init();
 	}
 	
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		
+
 		//System.out.println("bfdhjrf");
-		Bship_grid_global player_grid = new Bship_grid_global();
-		player_grid.setI_Board_size(20);
+//		Bship_grid_global player_grid = new Bship_grid_global();
+//		player_grid.setI_Board_size(20);
 		
 		resp.setContentType("application/json");
 		JSONObject jsonToSend;
 		jsonToSend = new JSONObject();
 		
-		jsonToSend.put("grid_size", player_grid.getI_Board_size());
+		//jsonToSend.put("grid_size", player_grid.getI_Board_size());
 		
 		PrintWriter out = resp.getWriter();
 		out.write(jsonToSend.toString());
+		
+		//pour recuperer la grid du js
+		String gridValue = req.getParameter("grid");
+		//System.out.println(gridValue);
+		JSONParser parser = new JSONParser();
+		Object obj = null;
+			try {
+				obj = parser.parse(gridValue);
+				
+			} catch (org.json.simple.parser.ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
 	}
 }
